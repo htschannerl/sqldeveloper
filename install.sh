ba9e5906902dabea2cc2954e967ba8e1a2a6e4eb
Henrique Tschannerl <h.tschannerl@gmail.com>
2022-12-29 10:58:36 -0300

    Changed the git URL

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /opt
-sudo git clone git@github.com:htschannerl/sqldeveloper.git
+sudo git clone https://github.com/htschannerl/sqldeveloper.git
 sudo cp /opt/sqldeveloper/sqldeveloper.desktop /usr/share/applications
